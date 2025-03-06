da3a1eb4e5f2a54f26ae26fbb23ab42dde8ba841
pipinokyo <cuneyt.omer@gmail.com>
2025-03-06 15:09:38 -0500

    modified task1.py

--- session1/homework/task1.py
+++ session1/homework/task1.py
@@ -2,9 +2,17 @@
 # Ask the user to enter a sentence. Calculate the length of the sentence.
 
 sentence = input("enter a sentence: ")
-# need to ask user to enter a sentence
-# and define a variable to save the input
+# # need to ask user to enter a sentence
+# # and define a variable to save the input
 length = len(sentence)
-# len() function is used to calculate the number of characters in the sentence 
+# # len() function is used to calculate the number of characters in the sentence 
 print("The length of the sentence is: ", length)
-# print the result
+# # print the result
+
+
+
+# if you want me count the words then the solutiouns is to apply split function
+# sentence = input("enter a sentence: ")
+# sentence = sentence.split(' ')
+# length = len(sentence)
+# print("there are:",length, "words in this sentence")
