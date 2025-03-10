89ab9a8724a29edf1ff295e3f12f33c1ff34a819
pipinokyo <cuneyt.omer@gmail.com>
2025-03-10 14:55:35 -0400

    added some new code to task5

--- session3/homework/task5.py
+++ session3/homework/task5.py
@@ -8,13 +8,22 @@
 # COD
 # CODE
 
-# take the input from the user
-word = input('Provide a word please: ')
-# set and empty variable so we can build add the loop results in to this
-pyramid = ""
-# with the for loop we go through each character 
-for i in word:
-    # here we add each character to each other and change the pyramid in every loop
-    pyramid += i
-    # we can print i to we can see how loop works
-    print(pyramid)
+# # take the input from the user
+# word = input('Provide a word please: ')
+# # set and empty variable so we can build add the loop results in to this
+# pyramid = ""
+# # with the for loop we go through each character 
+# for i in word:
+#     # here we add each character to each other and change the pyramid in every loop
+#     pyramid += i
+#     # we can print i to we can see how loop works
+#     print(pyramid)
+
+
+
+# this is not my code but putting it here to understand
+# Step 1: Take input from the user
+word = input("Enter a word: ")
+
+for i in range(1, len(word) + 1):
+    print(word[:i])
