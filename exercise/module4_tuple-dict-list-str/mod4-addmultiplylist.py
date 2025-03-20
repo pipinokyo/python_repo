00fc9b383e8f21400ae85f5907931098cfbda61a
pipinokyo <cuneyt.omer@gmail.com>
2025-03-19 21:32:41 -0400

    new topics

---
+++ exercise/module4_tuple-dict-list-str/mod4-addmultiplylist.py
@@ -0,0 +1,13 @@
+list_us = ['new york', 'chicago']
+list_uk = ['london', 'manchester']
+list_all = list_us + list_uk
+print(list_all)
+
+# the output of the above code will be:
+# ['new york', 'chicago', 'london', 'manchester']
+
+# we can also multiply a list by an integer to repeat its elements.
+list_numbers = [0, 1] * 10
+print(list_numbers)
+# the output of the above code will be:
+[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
