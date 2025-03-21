20554025a76fa09edde768b2e54d52891b9db1d6
pipinokyo <cuneyt.omer@gmail.com>
2025-03-20 23:08:06 -0400

    functions

---
+++ exercise/module5-functionsexceptions.py/into-funtions.py
@@ -0,0 +1,21 @@
+# functions are separated fragments of code.
+
+# they can be reused and called multiple times
+# they can take inputs (arguments) and return outputs (return values)
+# they can be used to organize code into logical blocks
+# they can be used to make code more readable and maintainable
+# they can be used to avoid code duplication
+# they can be used to improve code organization
+# they can be used to improve code readability
+# they can be used to improve code maintainability
+# they can be used to improve code reusability
+
+# let's create a simple function that prints a greeting message
+# a function definition starts with the keyword def, which is short for define.
+# Then after a space we provide the function's name.
+def greet():
+  print('Hello, my dear!')
+# this is a simple function that prints a greeting message
+# we can call this function by using its name followed by parentheses
+greet()  # this will print 'Hello, my dear!'
+# we can call this function as many times as we want
