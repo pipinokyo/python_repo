20d3d316c39ae9f75c25a6caff7d6d735e2341d1
pipinokyo <cuneyt.omer@gmail.com>
2025-03-28 16:57:31 -0400

    PCAP practise

---
+++ PCAP/q1.py
@@ -0,0 +1,19 @@
+# What is the expected output of the following snippet?
+i = 250
+while len(str(i)) > 72:
+    i *= 2
+else:
+    i //= 2
+print(i)
+
+'''
+visulation and explanation:
+1- Start:
+    i = 250
+    len(str(250)) = 3
+    Is 3 > 72? No → Skip loop, go to else
+2-Else:
+    i = 250 // 2 = 125
+3-Print:
+    Output: 125
+'''
