d5267b5e38f353ac237b32ad9ad54e6aa5d8d1cb
pipinokyo <cuneyt.omer@gmail.com>
2025-04-13 17:00:31 -0400

    PCAP

---
+++ PCAP/q30.py
@@ -0,0 +1,6 @@
+# Which of the following statements are true? (Select two answers)
+
+# A. The first argument of the open() function is an integer value.
+# B. The input() function reads data from the stdin stream.
+# C. There are three pre-opened file streams.
+# D. The readiness() function returns a string.
