b1d7735ac4f3d1ace7cea9e9572d44f4ef3dfef4
pipinokyo <cuneyt.omer@gmail.com>
2025-04-14 18:03:04 -0400

    PCAP

---
+++ PCAP/q31.py
@@ -0,0 +1,13 @@
+# What is the expected output of the following code?
+x = True  
+y = False  
+z = False  
+
+if not x or y:  
+    print(1)  
+elif not x or not y and z:  
+    print(2)  
+elif not x or y or not y and x:  
+    print(3)  
+else:  
+    print(4)
