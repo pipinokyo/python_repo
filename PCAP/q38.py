7175389d9f404ffd320e3d55371206c578fc337e
pipinokyo <cuneyt.omer@gmail.com>
2025-04-17 22:02:29 -0400

    PCAP

---
+++ PCAP/q38.py
@@ -0,0 +1,6 @@
+# Which of the following lines contain valid Python code? (Select two answers.)
+
+A. 1  |   lambda(a,b): return a if a < b else b 
+B. 1  |   lambda a,b: a if a < b else b 
+C. 1  |   lambda a,b = a if a < b else b 
+D. 1  |   lambda a,b: True 
