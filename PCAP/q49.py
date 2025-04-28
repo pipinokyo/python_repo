5c5b510160ce255a77c86b53e93bf21c2b523cf8
pipinokyo <cuneyt.omer@gmail.com>
2025-04-27 21:09:58 -0400

    PCAP

--- PCAP/q49.py
+++ PCAP/q49.py
@@ -10,3 +10,40 @@ class Test:
 x = Test()
 x.print()
 
+# Explanation:
+# Class Definition:
+# class Test: - Defines a new class named "Test"
+
+# def __init__(self, s='Welcome'): - Constructor method that initializes new objects
+
+# Takes a parameter s with default value 'Welcome'
+
+# self.s = s assigns the parameter to an instance variable
+
+# Methods:
+# def print(self): - Defines a method named "print"
+
+# print(self.s) - Prints the value of the instance variable s
+
+# Object Creation and Method Call:
+# x = Test() - Creates an instance of the Test class using default parameter
+
+# x.print() - Calls the print method on the x object
+
+# Visualization:
+# +---------------------+
+# |       Class Test    |
+# +---------------------+
+# | __init__(s='Welcome')|
+# | print()             |
+# +---------------------+
+#          ^
+#          |
+#          |
+# +---------------------+
+# |      Object x       |
+# +---------------------+
+# | s = 'Welcome'       |
+# +---------------------+
+# Expected Output:
+# When you run this code, it will output:  Welcome
