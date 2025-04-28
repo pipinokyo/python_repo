dfb9dee0e56380307cfec9e5c8e38eb336f7379c
pipinokyo <cuneyt.omer@gmail.com>
2025-04-28 17:45:33 -0400

    PCAP

---
+++ PCAP/q51.py
@@ -0,0 +1,11 @@
+# What would you insert instead ??? so that the program checks for even numbers?
+
+
+if ???:
+      print('x is an even number')
+
+A.   x % 1 ==  2
+B.   x % ‘even’ ==  True 
+C.   x % x ==  0
+D.   x % 2 ==  0
+E.   x % 2 ==  1
