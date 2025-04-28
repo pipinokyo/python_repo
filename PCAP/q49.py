8abedc665db21b623adf4c4f331cbbfd7631857f
pipinokyo <cuneyt.omer@gmail.com>
2025-04-27 21:08:54 -0400

    PCAP

---
+++ PCAP/q49.py
@@ -0,0 +1,12 @@
+# What is the expected output of the following code?
+
+class Test:
+    def __init__(self, s='Welcome'):
+        self.s = s
+        
+    def print(self):
+        print(self.s)
+
+x = Test()
+x.print()
+
