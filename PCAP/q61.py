21f8260bd08144880394134bdfdb2a5d8719dacd
pipinokyo <cuneyt.omer@gmail.com>
2025-05-29 21:38:47 -0400

    PCAP

---
+++ PCAP/q61.py
@@ -0,0 +1,27 @@
+# You develop a Python application for your company. You have the following code.
+
+def main(a, b, c, d):
+    value = a + b * c - d
+    return value
+
+# Which of the following expressions is equivalent to the expression in the function?
+
+
+# Python Operator Precedence:
+# Multiplication (*) has higher precedence than addition (+) and subtraction (-)
+
+# Addition and subtraction have the same precedence and evaluate left-to-right
+
+# Evaluation Order:
+# First: b * c (multiplication)
+
+# Then: a + (result of b * c) (addition)
+
+# Finally: (result of previous operation) - d (subtraction)
+
+# Equivalent Expressions:
+# Without seeing your specific options, the most likely equivalent expressions would be:
+
+# (a + (b * c)) - d - Explicitly showing the precedence with parentheses
+
+# a + (b * c) - d - Same as above but with minimal parentheses
