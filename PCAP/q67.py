84db2ae7301fa51e776f1764a83ac43773caadab
pipinokyo <cuneyt.omer@gmail.com>
2025-06-03 17:19:31 -0400

    PCAP

---
+++ PCAP/q67.py
@@ -0,0 +1,10 @@
+# What is the correct command to shuffle the following list?
+
+
+import random
+poeple = ['Joe', 'Bob', 'Sue', 'Sally']
+
+# A. shuffle(people)
+# B. people.shuffle( )
+# C. random,shuffle(people)
+# D. random.shuffleList(people) 
