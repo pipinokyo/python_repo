553b4fab89bf12d1d4605e22e12a126736d29522
pipinokyo <cuneyt.omer@gmail.com>
2025-06-02 20:17:13 -0400

    PCAP

---
+++ PCAP/q66.py
@@ -0,0 +1,8 @@
+# Which of the following expressions evaluates to True and raises no exception?
+'''
+A.  1  |  10 != '1' + '0'
+B.  1  |  'Al' * 2 != 2 * 'Al'
+C.  1  |   9' * 3 > '9' * 9
+D.  1  |  '9' * 1 < 1 * 2 
+'''
+
