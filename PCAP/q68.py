f4449bb0324ae384ac1b35ce210d8311abe6b41a
pipinokyo <cuneyt.omer@gmail.com>
2025-06-04 20:31:06 -0400

    PCAP

---
+++ PCAP/q68.py
@@ -0,0 +1,16 @@
+# What is the expected behavior of the following snippet?
+
+class Team:
+    def show_ID(self):
+        print(self.get_ID())
+
+    def get_ID(self):
+        return "anonymous"
+    
+class A(Team):
+    def get_ID(self):
+        return "Alpha"
+    
+
+a = A()
+a.show_ID()
