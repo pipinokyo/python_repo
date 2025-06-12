45f775eb57d1f56c87fd6270c4f343ba71ba01de
pipinokyo <cuneyt.omer@gmail.com>
2025-06-12 14:50:27 -0400

    PCAP

---
+++ PCAP/q76.py
@@ -0,0 +1,8 @@
+# What is the expected output of the following code?
+
+vect = ["alpha", "bravo", "charlie"]
+new_vect = filter(lambda s: s[-1].upper() in ["A", "O"], vect)
+for x in new_vect:
+    print(x[1], end="")
+
+    
